4540c97fa1e8cb2e0f0d03a572bcc58913e312b1
Nolan Conaway <nolanbconaway@gmail.com>
2020-11-27 14:12:29 -0500

    init, add server

---
+++ camera/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+RESOLUTION_MAPPING = {
+    "640x480": "640x480",
+    "480p": "640x480",
+    "1920x1080": "1920x1080",
+    "1080p": "1920x1080",
+    "1280x720": "1280x720",
+    "720p": "1280x720",
+}
+
+STORAGE = Path("/mnt/usb/camera")
+MODULE_DIR = Path(__file__).resolve().parent
