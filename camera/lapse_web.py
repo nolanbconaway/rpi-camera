179ac46fd9146d84825f43c3d126a0bb8320d4ce
Nolan Conaway <nolanbconaway@gmail.com>
2021-03-20 18:01:29 -0400

    support arbitrary lapse dirs

--- camera/lapse_web.py
+++ camera/lapse_web.py
@@ -1,6 +1,11 @@
-"""Provide a flask app that enables monitoring of the timelapse."""
+"""Provide a flask app that enables monitoring of the timelapse.
+
+Set an env variable, IMAGE_DIR, if you would like this to point to a specific directory.
+"""
 import base64
 import datetime
+import os
+from pathlib import Path
 
 import pytz
 from flask import Flask, jsonify, render_template
@@ -8,7 +13,7 @@ from gevent.pywsgi import WSGIServer
 
 from camera import config
 
-LAPSE_STORAGE = config.STORAGE / "lapses"
+DEFAULT_LAPSE_STORAGE = config.STORAGE / "lapses"
 
 
 app = Flask(__name__, template_folder="html")
@@ -16,11 +21,16 @@ app = Flask(__name__, template_folder="html")
 
 def get_latest() -> dict:
     """Return a dictionary of the info about the latest timelapse session."""
-    latest_lapse_dir = max([x for x in LAPSE_STORAGE.iterdir() if x.is_dir()])
-    images = [i for i in latest_lapse_dir.glob("*.jpg") if i.stat().st_size > 0]
+    if os.getenv("IMAGE_DIR") is None:
+        lapse_dir = max([x for x in DEFAULT_LAPSE_STORAGE.iterdir() if x.is_dir()])
+    else:
+        lapse_dir = Path(os.getenv("IMAGE_DIR"))
+
+    images = [i for i in lapse_dir.glob("*.jpg") if i.stat().st_size > 0]
     if not images:
         return dict()
     latest_image = max(images)
+    print(latest_image)
     return dict(
         image_path=latest_image,
         image_bytes_b64=base64.standard_b64encode(latest_image.read_bytes()).decode(),
@@ -29,9 +39,6 @@ def get_latest() -> dict:
             datetime.datetime.fromisoformat(latest_image.with_suffix("").name)
         ),
         lapse_n=len(images),
-        lapse_started_at=datetime.datetime.fromisoformat(
-            latest_lapse_dir.with_suffix("").name
-        ),
     )
 
 
@@ -44,7 +51,6 @@ def latest_api():
             image=latest["image_bytes_b64"],
             ts=latest["ts"],
             lapse_n=latest["lapse_n"],
-            lapse_started_at=latest["lapse_started_at"],
         )
     )
 
